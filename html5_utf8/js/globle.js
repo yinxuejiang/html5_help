1caa1e319cb50907c5d969cb231695dc9fe0e1a1
尹学江 <378915051@qq.com>
2016-07-02 20:21:08 +0800

    修改

--- html5_utf8/js/globle.js
+++ html5_utf8/js/globle.js
@@ -1,55 +1,54 @@
 /*
 自定义公用文件js
 */
-$(function(){
-	//加激活
-	$("body").on("click",".add-active",function(){
-		$(this).addClass("active");
-	});	
-	//去激活
-	$("body").on("click",".remove-active",function(){
-		$(this).removeClass("active");
-	});	
-	//切换激活
-	$("body").on("click",".toggle-active",function(){
-		$(this).toggleClass("active");
-	});	
-	
-	//父  加激活
-	$("body").on("click",".addp-active",function(){
-		$(this).parent().addClass("active");
-	});	
-	//父  去激活
-	$("body").on("click",".removep-active",function(){
-		$(this).parent().removeClass("active");
-	});	
-	//父  切换激活
-	$("body").on("click",".togglep-active",function(){
-		$(this).parent().toggleClass("active");
-	});	
-	
-	//关闭 父泡泡
-	$("body").on("click",".pub-closed-btn",function(){
-		$(this).parents(".pub-popup").fadeOut();
-	});
+$(function() {
+    //加激活
+    $("body").on("click", ".add-active", function() {
+        $(this).addClass("active");
+    });
+    //去激活
+    $("body").on("click", ".remove-active", function() {
+        $(this).removeClass("active");
+    });
+    //切换激活
+    $("body").on("click", ".toggle-active", function() {
+        $(this).toggleClass("active");
+    });
+
+    //父  加激活
+    $("body").on("click", ".addp-active", function() {
+        $(this).parent().addClass("active");
+    });
+    //父  去激活
+    $("body").on("click", ".removep-active", function() {
+        $(this).parent().removeClass("active");
+    });
+    //父  切换激活
+    $("body").on("click", ".togglep-active", function() {
+        $(this).parent().toggleClass("active");
+    });
+
+    //关闭 父泡泡
+    $("body").on("click", ".pub-closed-btn", function() {
+        $(this).parents(".pub-popup").fadeOut();
+    });
 });
 
 /*
 页面尺寸变化时，重新格式化的东西
 */
 
-$(function(){
-	$(window).resize(function(){
-	});
+$(function() {
+    $(window).resize(function() {});
 });
 
 /*
 点击空白处，要处理的东西
 */
 
-$(function(){
-	$(document).click(function(e) {
-        
+$(function() {
+    $(document).click(function(e) {
+
     });
 });
 
@@ -58,16 +57,8 @@ $(function(){
 /*
 测试js
 */
-$(function(){
-	
-	
-	
+$(function() {
+
+
+
 });
-
-
-
-
-
-
-
-
